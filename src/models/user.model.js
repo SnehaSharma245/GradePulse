230c334b2a874c3474f4e5e4e6671d6a6fa34876
Shubh Verma <shubhverma2003@gmail.com>
2025-04-15 22:20:54 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+// To be modified later
+
+import mongoose , { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "teacher", "institute"],
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verifyCode: {
+      type: String,
+      required: true,
+    },
+    verifyCodeExpiry: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
