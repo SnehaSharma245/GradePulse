f52e840d56cad0bfb241a5ceaba28dc82355959e
Sneha Sharma <snehav2109@gmail.com>
2025-04-29 19:05:24 +0530

    update middleware for invalid access

--- src/middleware.js
+++ src/middleware.js
@@ -3,25 +3,26 @@ import { getToken } from "next-auth/jwt";
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/login",
-    "/register",
-    "/",
-    "/verify/:path*",
-  ],
+  matcher: ["/dashboard/:path*", "/login", "/register", "/", "/verify/:path*"],
 };
 
 export async function middleware(request) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
+  const role = token?.role;
+
+  if (
+    role === "admin" ||
+    (role === "student" && url.pathname.startsWith("/teacher-dashboard"))
+  ) {
+    return NextResponse.redirect(new URL("/invalid-access", request.url));
+  }
 
   if (
     token &&
     (url.pathname.startsWith("/login") ||
       url.pathname.startsWith("/register") ||
-      url.pathname.startsWith("/verify")
-      )
+      url.pathname.startsWith("/verify"))
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
@@ -32,4 +33,3 @@ export async function middleware(request) {
 
   return NextResponse.next();
 }
-
