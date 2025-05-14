e50f9989c9f5564d6a187711fdb6cc30925f8bc7
Sneha Sharma <snehav2109@gmail.com>
2025-05-14 12:12:30 +0530

    update create-classroom api

--- src/app/api/create-classroom/route.js
+++ src/app/api/create-classroom/route.js
@@ -3,6 +3,7 @@ import Classroom from "@/models/classroom.model";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/options";
+import Teacher from "@/models/teacher.model";
 
 export async function POST(req) {
   try {
@@ -15,11 +16,20 @@ export async function POST(req) {
         { success: false, message: "Unauthorized" },
         { status: 401 }
       );
+
     const { classroomName, classroomCode, teacherName } = await req.json();
+
+    const teacher = await Teacher.findOne({ userId: user.id });
+    if (!teacher) {
+      return NextResponse.json(
+        { success: false, message: "Teacher not found" },
+        { status: 404 }
+      );
+    }
     await Classroom.create({
       classroomCode,
       classroomName,
-      teacher: user.id,
+      teacher: teacher._id,
     });
 
     return NextResponse.json(
