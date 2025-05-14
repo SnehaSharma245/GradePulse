939d6c9b17c3dc237cd737c92b59c3dc14dc39a8
Sneha Sharma <snehav2109@gmail.com>
2025-05-14 12:12:39 +0530

    update fetch-classroom api

--- src/app/api/fetch-classroom-details/route.js
+++ src/app/api/fetch-classroom-details/route.js
@@ -43,7 +43,7 @@ export async function POST(req) {
 
       const classroom = await Classroom.findOne({
         classroomCode: classroomCode,
-        teacher: user.id,
+        teacher: teacher._id,
       });
 
       if (!classroom) {
